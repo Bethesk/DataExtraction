f9c130627abd02f11450ce93e7c1d147dfebf924
Shin Kang <skang@sos.ca.gov>
2019-04-04 13:27:13 -0700

    Initial commits

--- DataExtraction/Program.cs
+++ DataExtraction/Program.cs
@@ -1,21 +1,86 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+
+/*References: https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/file-system/how-to-read-from-a-text-file
+ *           
+ *            https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/file-system/how-to-write-to-a-text-file
+ * 
+ * This program reads records from a csv file ("simpligov_data_dump_12_18_2018.csv") and write records into two differet files.
+ * 
+ */
+
+
 
 namespace DataExtraction
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static string originalFile = @"C:\Users\skang\Documents\TradeMarks\18000.csv";
+        static string llcPath = @"C:\Users\skang\Documents\TradeMarks\dump_file.csv";
+        static string tmPath = @"C:\Users\skang\Documents\TradeMarks\dump_fileTM.csv";
+        static string tm100Path = @"C:\Users\skang\Documents\TradeMarks\dump_fileTMTM10.csv";
+
+        public static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+            WriteData();
+            //ReadData();
+        }
+
+        public static void WriteData()
+        {
+            int count = 0;
+
+            string[] lines = System.IO.File.ReadAllLines(originalFile);
+
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(tmPath))
+            {
+                foreach (string line in lines)
+                {
+                    //Change a value to 'False' if a record should not contain 'LLC1' 
+                    if (line.Contains("LLC1") == false)
+                    {
+                        file.WriteLine(line);
+                        count++;
+
+                        Console.WriteLine(line);
+
+                    }
+
+                }
+
+            }
+            Console.WriteLine(count);
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+
         }
+        static void ReadData()
+        {
+            int count = 0;
+
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\skang\Documents\data_dump_12_18_2018.csv");
+
+            foreach (string line in lines)
+            {
+
+                if (line.Contains("LLC1") == true)
+                {
+                    Console.WriteLine("Word found!: " + line);
+                    count++;
+                }
+
+            }
+            Console.WriteLine(count);
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+
+        }
+
     }
 }
